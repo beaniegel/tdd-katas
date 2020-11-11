12c5c256f8587c0f71ddf6c4bc7c59556811182b
beaniegel <mail@jelena.io>
2020-11-11 01:41:30 +0000

    add input validation

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -10,7 +10,17 @@ class StringCalculator
 {
     public function add(string $string): int
     {
+        $this->validateInput($string);
         $numbers = \explode(",", $string);
         return \array_sum($numbers);
+    }
+
+    private function validateInput(string $string): void
+    {
+        $invalidInput = \preg_match("/[^0-9,]+/", $string);
+
+        if ($invalidInput) {
+            throw new InvalidArgumentException('String should only contain numbers separated by a comma');
+        }
     }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -7,6 +7,14 @@ use PHPUnit\Framework\TestCase;
 /** @covers \jellybean\StringCalculator */
 class StringCalculatorTest extends TestCase
 {
+    private StringCalculator $calculator;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->calculator = new StringCalculator();
+    }
+
     public function basicNumberStringProvider() {
         return [
             ["", 0],
@@ -20,9 +28,30 @@ class StringCalculatorTest extends TestCase
     /** @dataProvider basicNumberStringProvider */
     public function testAdd(string $input, int $expected): void
     {
-        $calculator = new StringCalculator();
-        $actual = $calculator->add($input);
+        $actual = $this->calculator->add($input);
 
         self::assertSame($expected, $actual);
+    }
+
+    public function invalidInputProvider()
+    {
+        return [
+            ["just,some,words"],
+            ["end of line".PHP_EOL],
+            ["&*(@&#$%#%@$#:<{}["],
+            ["ALSO,TEST,C4PS"],
+            ["125^93^385^0212"]
+        ];
+    }
+
+    /**
+     * @dataProvider invalidInputProvider
+     */
+    public function testException(string $invalidInput): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('String should only contain numbers separated by a comma');
+
+        $this->calculator->add($invalidInput);
     }
 }
